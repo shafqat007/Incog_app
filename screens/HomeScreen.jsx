54d55b9117016b0683d96ac7c4ded84fd500aeeb
shafqat007 <shafqatnawazchy@gmail.com>
2024-04-18 03:21:06 +0600

    GroupCreates

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react';
+import { View, Text, SafeAreaView, Image, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native';
+import { useSelector } from 'react-redux';
+import { ChatPlus } from '../assets';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { firestoreDB } from '../config/firebase.config';
+import { orderBy, query, collection, onSnapshot } from 'firebase/firestore';
 
 const HomeScreen = () => {
   return (
